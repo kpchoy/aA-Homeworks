1faa2cc0ffeab7c3794d5f9cc71c42297d339a9d
Kevin Choy <kevinchoy22@gmail.com>
2018-06-21 23:28:03 -0700

    created Queue class

--- W1D5/DIY_ADTs.rb
+++ W1D5/DIY_ADTs.rb
@@ -17,6 +17,35 @@ class Stack
   end
 end
 
-stack = Stack.new([1,2,3,4,5])
-stack.pop
-p stack.peek
+# stack = Stack.new([1,2,3,4,5])
+# stack.pop
+# p stack.peek
+
+class Queue
+  attr_reader :ivar
+
+  def initialize(ivar)
+    @ivar = ivar
+  end
+
+  def enqueue(el)
+    @ivar = [el].concat(@ivar)
+  end
+
+  def dequeue
+    @ivar = @ivar[0..-2]
+  end
+
+  def peek
+    @ivar[-1]
+  end
+end
+
+que = Queue.new([1,2,3,4])
+p que.ivar
+que.enqueue(5)
+p que.ivar
+que.dequeue
+p que.ivar
+p que.peek
+p que.ivar
