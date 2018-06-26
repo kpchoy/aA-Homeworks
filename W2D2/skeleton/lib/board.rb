89ccc1bcb787479aecb76cc6f22e84aae07a144a
Kevin Choy <kevinchoy22@gmail.com>
2018-06-26 09:44:36 -0700

    passed specs

--- W2D2/skeleton/lib/board.rb
+++ W2D2/skeleton/lib/board.rb
@@ -32,14 +32,37 @@ class Board
   def make_move(start_pos, current_player_name)
     stones = self.cups[start_pos]
     self.cups[start_pos] = []
-    stones.each_with_index do |stone, idx|
-      next if (idx + 1 + start_pos) > 13
-      self.cups[idx+start_pos+1] << stone
+
+    current_idx = start_pos
+
+    until stones.empty?
+      current_idx += 1
+      current_idx = 0 if current_idx > 13
+
+      if current_idx == 6
+        self.cups[6] << stones.pop if current_player_name == @name1
+      elsif current_idx == 13
+        self.cups[13] << stones.pop if current_player_name == @name2
+      else
+        self.cups[current_idx] << stones.pop
+      end
     end
-    p self.cups[13]
+
+    render
+    next_turn(current_idx)
+
+
   end
 
   def next_turn(ending_cup_idx)
+
+    if ending_cup_idx == 6 || ending_cup_idx == 13
+      :prompt
+    elsif @cups[ending_cup_idx].count == 1
+      :switch
+    else
+      ending_cup_idx
+    end
     # helper method to determine whether #make_move returns :switch, :prompt, or ending_cup_idx
   end
 
@@ -52,8 +75,18 @@ class Board
   end
 
   def one_side_empty?
+    @cups.take(6).all? { |cup| cup.empty? } ||
+    @cups.drop(6).all? { |cup| cup.empty? }
   end
 
   def winner
+    player1_count = @cups[6].count
+    player2_count = @cups[13].count
+
+    if player1_count == player2_count
+      :draw
+    else
+      player1_count > player2_count ? @name1 : @name2
+    end 
   end
 end
